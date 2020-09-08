92fa2875691fc7552b1cd8f9cb468f00d839837c
riteshtikare <60392084+riteshtikare@users.noreply.github.com>
2020-09-08 19:55:30 +0530

    3nd commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ dropmenu.addEventListener('click',function(){
   if (x.style.display === "none") {
     // x.style.display = "block";
     dropmenu.style.marginTop ="-10%";
-    selection.style.height = "85%";
+    selection.style.height = "40%";
   } else {
     // x.style.display = "none";
     selection.style.height = "initial";
